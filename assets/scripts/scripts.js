b1e3fa24c2413a4a07e2f48c66a5714de2d397d6
chrisfaux95 <chrisfaux95@gmail.com>
2020-08-07 14:03:31 -0400

    created function for ajax call to weather api

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,12 @@
+var key = "fc9370f26192d02c666de9df21b1cac5"
+
+function getWeather(cityName) {
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + key;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(response => {
+        pushWeather(response);
+    })
+}
 
